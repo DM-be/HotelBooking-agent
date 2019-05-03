b8212b88f3c761dd3fbd260bde38499b846aa9b2
Dennis Morent - Stardekk <Dennis.Morent@stardekk.local>
2019-05-03 10:51:13 +0200

    added basic responses

--- Dialogs/ConfirmOrder/ConfirmOrderResponses.cs
+++ Dialogs/ConfirmOrder/ConfirmOrderResponses.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelBot.Dialogs.RoomDetail;
+using Microsoft.Bot.Builder.TemplateManager;
 
 namespace HotelBot.Dialogs.ConfirmOrder
 {
-    public class ConfirmOrderResponses
+    public class ConfirmOrderResponses: TemplateManager
     {
+        private static readonly LanguageTemplateDictionary _responseTemplates = new LanguageTemplateDictionary
+        {
+            ["default"] = new TemplateIdMap
+            {
+                
+
+
+            }
+
+        };
+
+        public ConfirmOrderResponses()
+        {
+            Register(new DictionaryRenderer(_responseTemplates));
+        }
+
+        public class ResponseIds
+        {
+            
+        }
     }
 }
